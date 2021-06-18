763add8348f944d7b2e1f3c7bef8b379aa7e0191
Synfenzo <mikecao060211@hotmail.com>
2021-06-18 21:45:29 +0200

    fixed code

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
     var b;
     // let summa = a + b;
     //console.log(summa);
+    var volym = function (radie, hojd) {
+        return Math.PI * (radie) * (radie) * (hojd) / 3;
+    };
     (_a = document.querySelector("#summera")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
         a = document.querySelector("#a").value;
         b = document.querySelector("#b").value;
-        var summa = (Math.PI * parseInt(a) * parseInt(a) * parseInt(b) / 3);
+        var summa = volym(parseInt(a), parseInt(b));
+        // for(let i = 0; i<10;i++){
+        //     let x = i * 10;
+        //     console.log(x);
+        // }
         var svar = document.querySelector("#summa").textContent = summa.toString();
     });
 });
--- app.ts
+++ app.ts
@@ -9,14 +9,22 @@ document.addEventListener("DOMContentLoaded",() => {
 
     //console.log(summa);
 
-
+        let volym = (radie:number,hojd:number) => {
+            return Math.PI * (radie) * (radie) * (hojd) / 3;
+        }
 
         document.querySelector("#summera")?.addEventListener("click",() => {
 
             a = (document.querySelector("#a") as HTMLInputElement).value;
             b = (document.querySelector("#b") as HTMLInputElement).value;
 
-            let summa = (Math.PI * parseInt(a) * parseInt(a) * parseInt(b) / 3);
+            let summa = volym(parseInt(a),parseInt(b));
+
+            
+            // for(let i = 0; i<10;i++){
+            //     let x = i * 10;
+            //     console.log(x);
+            // }
             
             let svar = (document.querySelector("#summa")as HTMLElement).textContent = summa.toString();
 
